5771fddd639a1887484c9ceafd2cace9670b0c9e
Mederic <kottomouyema.m@gmail.com>
2024-05-25 16:20:59 +0200

    Quete ASP.Net Templating

---
+++ Controllers/ExpendituresController.cs
@@ -0,0 +1,32 @@
+﻿using ASP.Net_Templating.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Templating.Controllers
+{
+    public class ExpenditureController : Controller 
+    {
+        [Route("/{controller}/Display")]
+        public IActionResult DisplaySumExpenditures()
+        {
+            List<Expenditure> depenses = new List<Expenditure>();
+
+            depenses.Add(new Expenditure { Id = 1, Date = new DateOnly(2024, 05, 2), Name = "Chips", Price = 2.35M });
+            depenses.Add(new Expenditure { Id = 2, Date = new DateOnly(2024, 05, 18), Name = "Saumon", Price = 13.80M }); 
+            depenses.Add(new Expenditure { Id = 3, Date = new DateOnly(2024, 05, 24), Name = "Sneakers", Price = 5.75M }); 
+
+            decimal sommeDepenses = 0; 
+
+            for (int i = 0; i < depenses.Count; i++)
+            {
+                // Arrondi de 2 chiffres après la virgule
+                decimal arrondiDepense = Math.Round(depenses[i].Price, 2);  
+
+                sommeDepenses += arrondiDepense;
+            }
+
+            ViewData["somme"] = sommeDepenses; 
+
+            return View(depenses); 
+        }
+    }
+}
---
+++ Models/Expenditure.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.Net_Templating.Models
+{
+    public class Expenditure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly Date { get; set; }
+        public decimal Price { get; set; }
+    }
+}
